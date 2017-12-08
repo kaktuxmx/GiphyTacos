6037cb7e45caab420d896de7a1ef33f27b430d7f
kaktuxmx <kaktuxsql@gmail.com>
2017-12-07 22:43:34 -0700

    Adding HMTL and JS, still working on this

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,24 @@
 // This is the script for my Giphy Tacos
 //test
+
+  var foods = ["Korean", "Tacos","Ramen"]
+
+  function displaygiphysfood() {
+
+    var food = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/" + food +"?api_key=mJhdpWB6O4RgVbVrwsAb1qbW9DqSZoZm"
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    });.done(function(response) {
+
+      var foodDiv = $("<div class='food'>");
+    });
+  };
+
+    function renderbuttons() {
+      $("#buttons-view").empty();
+
+      for (var i = 0; i <)
+    }
