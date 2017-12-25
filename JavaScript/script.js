397fdbbdcf803539147232a30d4417b529c12792
kaktuxmx <kaktuxsql@gmail.com>
2017-12-25 16:10:59 -0700

    This is final for second revision. Finally is working.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,42 +1,40 @@
-// This is the script for my Giphy Tacos
-//test
-  alert("puto");
+  //This is the click event button to grab the input from html
+  $("button").on("click", function() {
+    //Prevents the page to reload
+    event.preventDefault();
 
-  var foods = ["Korean", "Tacos","Ramen"]
+    //This variable will hold the input from the "free text" after the submit button is click.
+    var food = $("#food-input").val().trim();
+    console.log(food);
 
-  function displaygiphysfood() {
+    // This is the Ajax URL with the key provided by Giphy.com dev site.
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=mJhdpWB6O4RgVbVrwsAb1qbW9DqSZoZm";
+      console.log(queryURL);
 
-    var food = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/" + food +"?api_key=mJhdpWB6O4RgVbVrwsAb1qbW9DqSZoZm"
-
+    // Ajax Call, remember, is url, method, then done(function(response)) then your funtion.
     $.ajax({
       url: queryURL,
       method: "GET"
-    });.done(function(response) {
+    }).done(function(response) {
 
-      var foodDiv = $("<div class='food'>");
+        // This will save the response of the from the AJAX call.
+      var foodselected = response.data;
+      // Loop trought the response variable, to show all the jsons received.
+      for (var i = 0; i < foodselected.length; i++) {
 
-      var foodselected = response.selected
+          //Create a new div for the responses.
+        var gifDiv = $("<div class='item'>");
 
-      foodDiv.append(foodselected);
+         // Create a new img source within the new divs created.
+        var foodgifs = $("<img>");
 
-    });
-  };
+            // Providing the attribute to the htmls as src.
+          foodgifs.attr("src", foodselected[i].images.fixed_height.url);
 
-    function renderbuttons() {
-      $("#buttons-view").empty();
-
-      for (var i = 0; i < foods.length; i++ ) {
-
-      var f = $("<button>");
-
-      f.addClass("food");
-
-      f.attr("add-food");
-
-      f.text(food[i]);
-
-      $("buttons-view").append(f);
-
-      };
-    };
+            // Prepend the <img> html within the div.
+            gifDiv.prepend(foodgifs);
+            // prepend the Div within the div from the HTML page.
+            $("#foodinputsgifs").prepend(gifDiv);
+};
+});
+});
